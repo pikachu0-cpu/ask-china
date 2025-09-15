657b11ea88b5ed9c06b9c6bc40021a109964a94f
chuhy <chu.hongyang@cgtn.com>
2025-09-15 10:20:17 +0800

    update

--- packages/pure/utils/server.ts
+++ packages/pure/utils/server.ts
@@ -4,14 +4,6 @@ type Collections = CollectionEntry<CollectionKey>[]
 
 export const prod = import.meta.env.PROD
 
-/** Note: this function filters out draft posts based on the environment */
-export async function getBlogCollection(contentType: CollectionKey = 'blog') {
-  return await getCollection(contentType, ({ data }: CollectionEntry<typeof contentType>) => {
-    // Not in production & draft is not false
-    return prod ? !data.draft : true
-  })
-}
-
 function getYearFromCollection<T extends CollectionKey>(
   collection: CollectionEntry<T>
 ): number | undefined {
